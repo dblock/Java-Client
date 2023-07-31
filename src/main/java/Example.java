686eb4ff5e538b518e4aaa7b6583ccae83069a3a
dblock <dblock@amazon.com>
2023-07-31 11:59:25 -0400

    Serialize requests to JSON.
    Signed-off-by: dblock <dblock@amazon.com>

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,7 +1,15 @@
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Objects;
 
 import org.apache.http.HttpHost;
+import org.opensearch.client.json.JsonpMapper;
+import org.opensearch.client.json.JsonpSerializable;
+import org.opensearch.client.json.JsonpUtils;
+import org.opensearch.client.json.jackson.JacksonJsonProvider;
+import org.opensearch.client.json.jackson.JacksonJsonpGenerator;
+import org.opensearch.client.json.jackson.JacksonJsonpMapper;
+import org.opensearch.client.json.jsonb.JsonbJsonpMapper;
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.client.opensearch._types.OpenSearchException;
 import org.opensearch.client.opensearch.core.IndexRequest;
@@ -16,11 +24,21 @@ import org.opensearch.client.transport.aws.AwsSdk2TransportOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import jakarta.json.stream.JsonGenerator;
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.regions.Region;
 
 public class Example {
+
+    static String toJson(JsonpSerializable obj) {
+        StringWriter stringWriter = new StringWriter();
+        JsonbJsonpMapper mapper = new JsonbJsonpMapper();
+        JsonGenerator generator = mapper.jsonProvider().createGenerator(stringWriter);
+        mapper.serialize(obj, generator);
+        generator.close();
+        return stringWriter.toString();
+    }
 
     public static void main(final String[] args) throws IOException, InterruptedException {
         Logger logger = LoggerFactory.getLogger(Example.class);
@@ -51,7 +69,16 @@ public class Example {
 
             // create the index
             String index = "movies";
-            CreateIndexRequest createIndexRequest = new CreateIndexRequest.Builder().index(index).build();
+            IndexSettings settings = new IndexSettings.Builder()
+                .numberOfShards("2")
+                .numberOfReplicas("1")
+                .build();
+            CreateIndexRequest createIndexRequest = new CreateIndexRequest.Builder()
+                .index(index)
+                .settings(settings)
+                .build();
+
+            logger.info("JSON: {}", toJson(createIndexRequest));
 
             try {
                 client.indices().create(createIndexRequest);
@@ -62,6 +89,9 @@ public class Example {
                         .index(index)
                         .settings(indexSettings)
                         .build();
+
+                logger.info("JSON: {}", toJson(putSettingsRequest));
+
                 client.indices().putSettings(putSettingsRequest);
             } catch (OpenSearchException ex) {
                 final String errorType = Objects.requireNonNull(ex.response().error().type());
@@ -77,6 +107,8 @@ public class Example {
                     .id("1")
                     .document(movie)
                     .build();
+
+            logger.info("JSON: {}", toJson(indexRequest));
             client.index(indexRequest);
 
             // wait for the document to index
